fd3852467132d8b97b51b74f2d19b578c1ed0934
Kshitiz <70872155+theNovahkiin@users.noreply.github.com>
2023-11-02 20:45:42 +0545

    Add files via upload
    Updating code to include object-oriented approach to model a ball and spawning user-given number of balls onto the canvas with randomized colors of the balls.

--- script.js
+++ script.js
@@ -2,27 +2,49 @@ const canvas = document.getElementById("cnv");
 const context = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+const colors = ["red", "teal", "green", "orange", "purple", "white", "skyblue", "lavender", "grey", "blueviolet", "brown", "gold"];
 
-let xPos = Math.random() * window.innerWidth;
-let yPos = Math.random() * window.innerHeight;
-let xSpeed = 5;
-let ySpeed = 5;
-let radius = 30;
-
-function ball(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    requestAnimationFrame(ball);
-    context.fillStyle = 'teal';
-    context.beginPath();
-    context.arc(xPos, yPos, radius, 0, Math.PI * 2);
-    context.fill();
-    xPos += xSpeed;
-    yPos += ySpeed;
-    if (xPos + radius > window.innerWidth || xPos - radius < 0){
-        xSpeed = -xSpeed;
+class Ball{
+    constructor(xPos, yPos, xSpeed, ySpeed, radius, color){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+        this.radius = radius;
+        this.color = color;
     }
-    if (yPos + radius > window.innerHeight || yPos - radius < 0){
-        ySpeed = -ySpeed;
+        draw() {
+            context.fillStyle = `${this.color}`;
+            context.beginPath();
+            context.arc(this.xPos, this.yPos, this.radius, 0 , Math.PI * 2);
+            context.fill();
+        }
+        move(){
+            if (this.xPos + this.radius > innerWidth || this.xPos - this.radius < 0){
+                this.xSpeed = -this.xSpeed;
+            }
+            if (this.yPos + this.radius > innerHeight || this.yPos - this.radius < 0){
+                this.ySpeed = -this.ySpeed;
+            }
+            this.xPos += this.xSpeed;
+            this.yPos += this.ySpeed;
+            this.draw();
+        }
+}
+var ballAmount = Number(window.prompt("Number of balls to juggle??"));
+var balls = [];
+
+for (let i = 0; i < ballAmount; i++){
+    var ball = new Ball(Math.random() * innerWidth, Math.random() * innerHeight, 4, 4, 40, colors[Math.ceil(Math.random() * colors.length)]);
+    balls.push(ball);
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, innerWidth, innerHeight);
+    for (let i = 0;  i < balls.length; i++) {
+        balls[i].move();
     }
 }
-ball();
+
+animate();
